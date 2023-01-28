c78134479b8a0e60790a1d8396255b2acd66f289
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-01-28 15:57:39 +0530

    install the react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <h3>Portfolio web page</h3>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Contact
+      <h1>connect me</h1>
+    </div>
+  )
+}
+
+export default Contact;
