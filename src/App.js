019057201863e34d4b6a99964ff45596a57112c7
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-03-24 11:09:16 +0530

    set the layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import './App.css';
-import Contact from './pages/Contact';
-
+import React from "react";
+import  Header  from "./components/Header/Header";
+import "./App.css";
+import { ThemeContext } from "./Context/theme";
+import  About  from "./components/About/About";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import  Github  from "./components/About/Github";
+import  Projects  from "./components/Projects/Projects";
+import  Contact  from "./components/Contact/Contact";
+import  Footer  from "./components/Footer/Footer";
+import  ScrollToTop  from "./components/ScrollToTop/ScrollToTop";
 function App() {
+  const [{ themename }] = React.useContext(ThemeContext);
+  React.useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
   return (
-    <div>
-      <h1>hello world</h1>
-      <h3>Portfolio web page</h3>
-      <Contact />
+    <div id="top" className={`${themename} app`}>
+      <section id="#home">
+        <Header />
+      </section>
+      <main>
+        <About />
+        <Github />
+        <section id="#projects">
+          <Projects />
+        </section>
+        <section id="#contact">
+          <Contact />
+        </section>
+      </main>
+      <Footer />
+      <ScrollToTop />
     </div>
   );
 }
-
 export default App;
