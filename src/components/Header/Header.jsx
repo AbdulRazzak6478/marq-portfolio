7f126f22549f413f7318250d6dd05294ccd9985e
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-03-25 10:51:24 +0530

    Added Header Component and make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import  Navbar  from "../Navbar/Navbar";
+import { ThemeContext } from "../../Context/theme";
+import Logo from "../../assets/logo-img.webp";
 
+import "./Header.css";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [{ themename }] = React.useContext(ThemeContext);
 
-export default Header
+  return (
+    <>
+      <header className={"header center " + themename}>
+        <h3>
+          <a href="#home" className="link">
+           Abdul Razzak
+          </a>
+        </h3>
+        <Navbar />
+      </header>
+    </>
+  );
+};
+ export default Header;
