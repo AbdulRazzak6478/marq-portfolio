8539316549a325b5462df9004d26c45aa08a541d
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-03-27 01:07:20 +0530

    contact section added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import { VscGithub } from "react-icons/vsc";
+import { CgMail } from "react-icons/cg";
+import { BsInstagram, BsFillTelephoneFill } from "react-icons/bs";
+import { ThemeContext } from "../../Context/theme";
 
+import { FaLinkedin } from "react-icons/fa";
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [{ themename }] = React.useContext(ThemeContext);
 
-export default Contact
+  return (
+    <>
+      <div className="section">
+        <h2 className="section__title" data-aos="fade-right">
+          Get in <span className="different">Touch</span>
+        </h2>
+        <div className={"contactcontainer " + themename} data-aos="fade-right">
+          <a
+            href="https://www.linkedin.com/in/abdul-razzak-a808571b4/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin className="linkedin" />
+          </a>
+          <a
+            href="https://github.com/AbdulRazzak6478"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <VscGithub className="github" />
+          </a>
+          <a
+            href="mailto:abdulrazzak4186@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <CgMail className="email" />
+          </a>
+          <a
+            href="https://www.instagram.com/marq_immu6478"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsInstagram className="twitter" />
+          </a>
+          <a href="tel:+919515426478" target="_blank" rel="noreferrer">
+            <BsFillTelephoneFill className="phone" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Contact;
